1ce21d28e1aec2c8a278f7fad05b6db29ddd45fd
Bilal-Siddiqe <dev.bilalsiddique>
2025-01-13 22:03:18 +0500

    Changes Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>
+    <div className='d-flex justify-content-center align-items-center mt-5'>
 
-      <h1 className='text-center'> App is Running</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim harum, ullam facere corrupti dolor esse dignissimos nemo, temporibus doloremque et modi dolores cumque dolorem nisi similique, ex eum veritatis? Sint.</p>
+      <h1 className='text-center mt-5'> Website Under Maintenance</h1>
+      
 
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,9 @@ import "./Navbar.css"
 import PropTypes from 'prop-types';
 
 export default function Navbar(props) {
-
-
     return (
         <div className='navbar'>
-
-            <h1>{props.title}</h1>
-         
+            <h1>{props.title}</h1>     
         </div>
     )
 }
@@ -17,7 +13,6 @@ export default function Navbar(props) {
 Navbar.defaultProps = {
     title:"Hello",
 }
-
 Navbar.propTypes = {
     title: PropTypes.bool,
 
